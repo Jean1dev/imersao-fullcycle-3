126d7c12235d34b5f1fe0e570800e4a86852f6ff
Jeanluca <jeanlucafp@gmail.com>
2023-02-16 09:48:03 -0300

    concluido

--- docker-desafio/nginx-challenge/node/index.js
+++ docker-desafio/nginx-challenge/node/index.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -5,20 +6,36 @@ const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database:'nodedb'
+    database: 'nodedb'
 };
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
 
-// const sql = `INSERT INTO people(name) values('Wesley')`
-// connection.query(sql)
-// connection.end()
+app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config)
+
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.message);
+            return res.send('problema para conecar no banco');
+        }
+
+        console.log('connected as id ' + connection.threadId);
+        connection.query('CREATE TABLE IF NOT EXISTS people(name VARCHAR(255))', () => {
+            connection.query(`INSERT INTO people(name) values('Wesley')`, () => {
+                connection.query('SELECT * FROM people', (error, results, fields) => {
+                    if (error) throw error;
+
+                    console.log('ok', results)
+
+                    connection.end()
+                    res.send(results)
+                })
+            })
+        })
+    });
 
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('Rodando na porta ' + port)
 })
